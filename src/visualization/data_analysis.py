4568030080bb55ec3ade7820ce80d73b3668c3b0
Gentian Shatri <gentianshatri74@gmail.com>
2023-01-08 18:11:47 +0100

    New plots

--- src/visualization/data_analysis.py
+++ src/visualization/data_analysis.py
@@ -1,75 +1,72 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-
-year = [str(x+1) for x in range(2009,2022)]
-
-df = pd.read_pickle("data/arxiv_id_total_citation_th.pkl")
-
-df_year = pd.read_pickle('data/arxiv_id_citation_year_th.pkl')
-
-df.columns = ['arxiv_id' if x=='ArXiV ID' else x for x in df.columns]
+import glob
+import json
 
 
-mean_citation_tot = df['Number of total citations'].mean()
-print('Total citation mean :', mean_citation_tot)
+def dist_citations(folder_path,start_year,end_year):
 
-mean_per_year = [(y,df_year[y].mean()) for y in year]
-print(mean_per_year)
+    stat_table = df['Total'].describe()
+    stat_table.to_excel(f"{folder_path}_stat_table_{start_year}_{end_year}.xlsx")
+    print(stat_table)
+    tot_citations = np.array(df['Total'].to_list())
 
-df_sorted = df.sort_values(by=['Number of total citations'], ascending=False)
-df_test = df.merge(df_year, on='arxiv_id')
-
-df_test.columns = ['Total' if x == 'Number of total citations' else x for x in df_test.columns]  
-
-print(df_test.head())
-
-df_test.to_pickle("data/final_hep_th.pkl")
-
-cit_11_13 = df_year[['2011','2012','2013']].sum(axis=1).to_list()
-print('Percentage of articles (2011-13) with number of citations bigger than average : ', 100*sum(i>mean_citation_tot for i in cit_11_13)/len(df_year))
+    plt.hist(tot_citations,bins = 100,histtype='step',density=True)
+    plt.xlabel('Number of citations')
+    plt.yscale("log")
+    plt.xscale("log")
+    plt.xlim(right=3000)
+    plt.title("Distribution of the number of citations for hep-th articles (2010-15)")
+    plt.savefig(f"{folder_path}citation_distribution_{start_year}_{end_year}.png")
 
 
-cit_11_15 = df_year[['2011','2012','2013','2014','2015']].sum(axis=1).to_list()
-print('Percentage of articles (2011-15) with number of citations bigger than average : ', 100*sum(i>mean_citation_tot for i in cit_11_15)/len(df_year))
+def excel(folder_path, start_year,end_year):
+    df.to_excel(f"{folder_path}total_citation_year_th_{start_year}_{end_year}.xlsx")
+    print("Excel file created")
 
+def report_json():
 
-col = df['Number of total citations']
+    reports = glob.glob("reports/*.json")
+    classifier = []
+    precision = {}
+    recall = {}
+    f1_score = {}
+    f1_micro = {}
 
-count_infmean = 100*(np.count_nonzero(col <= mean_citation_tot))/len(col)
-count_supmean_inf100 = 100*(np.count_nonzero((col>mean_citation_tot) & (col<100))/len(col))
-count_sup100 = 100*(np.count_nonzero(col >= 100))/len(col)
+    for i, report in enumerate(reports):
+        with open(report, "r") as file:
+            data = json.load(file)
+            classifier.append(data['Estimator'])
+            precision[i] = [100*data['A']['precision'],100*data['B']['precision'],100*data['C']['precision']]
+            recall[i] = [100*data['A']['recall'],100*data['B']['recall'],100*data['C']['recall']]
+            f1_score[i] = [100*data['A']['f1-score'],100*data['B']['f1-score'],100*data['C']['f1-score']]
+            f1_micro[data['Estimator']] = 100*data['f1_micro']
 
+         
+    abs = ['[0,25)','[25,100]','> 100 citations']
 
-print("Between 0 and 32 citations : ", count_infmean)
-print("Between 32 and 100 citations : ", count_supmean_inf100)
-print("Greater than 100 citations : ", count_sup100)
+    plt.figure(0)
+    df_micro = pd.DataFrame({'Classifiers':list(f1_micro.values())},index=classifier)
+    cp = df_micro.plot.bar(rot=0,legend=False)
+    plt.title('F1 micro')
+    plt.savefig('reports/figures/f1_micro_all_classifiers.png')
+    print('reports/figures/f1_micro_all_classifiers.png has been saved')
+    
+    for i, clf in enumerate(classifier):
+        plt.figure(i+1)
+        df = pd.DataFrame({'Precision':precision[i],'Recall':recall[i],'F1':f1_score[i]},index=abs)
+        ax = df.plot.bar(rot=0)
+        plt.title(f'{clf} Performance')
+        plt.savefig(f'reports/figures/{clf}_cat_prf.png')
+        print(f'reports/figures/{clf}_cat_prf.png has been saved')
 
+        
+    
 
-''' mean_per_year = [(y,df_year[y].mean()) for y in year]
-print(mean_per_year)
+if __name__ == "__main__":
+    df = pd.read_pickle("data/arxiv_id_total_citation_year_th_2010_2015.pkl")
+    #excel("reports/",2010,2015)
+    #dist_citations("reports/figures/",2010,2015)
 
-plt.hist(df_year['2010'],density=True,bins=100,range=(0,300),histtype=u'step')
-
-plt.xlabel('Number of citations')
-plt.yscale("log")
-plt.xlim(left=0)
-plt.title("Distribution of the number of citations for hep-th articles in 2010")
-#plt.show()
-plt.savefig("data/hep_th_distribution_citation_2010.png") '''
-
-''' df_sorted = df.sort_values(by=['Number of total citations'], ascending=False)
-df_ysort = df_year.sort_values(by=['2010','2011','2012','2013'], ascending=False)
-df_test = df.merge(df_year, on='arxiv_id')
-
-plt.hist(df['Number of total citations'],density=True,bins=100,range=(0,300),histtype=u'step')
-
-plt.xlabel('Number of citations')
-plt.yscale("log")
-plt.xlim(left=0)
-plt.title("Distribution of the number of citations for hep-th articles (2010-15)")
-
-plt.savefig("data/hep_th_distribution_citation.png") ''' 
-
-
-
+    report_json()
